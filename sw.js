82ee16c0d9ba4cd10d1d41fbae38c2bdf7d69b3d
ym855076-commits <ym855076@gmail.com>
2026-09-18 23:44:23 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'arza-komekchi-v1';
+const FILES = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const fetchPromise = fetch(e.request)
+        .then((res) => {
+          if (res && res.status === 200 && e.request.url.startsWith(self.location.origin)) {
+            const copy = res.clone();
+            caches.open(CACHE).then((cache) => cache.put(e.request, copy));
+          }
+          return res;
+        })
+        .catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
